e6c391e9e0082dcf4625ca0577b6a0c805725df4
Yashvi04 <47842343+YashviSaxena@users.noreply.github.com>
2022-05-21 01:50:36 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,16 @@ const FooterDiv = styled.div`
   justify-content: center;
   align-items: center;
   position: absolute;
-  bottom: -40px;
+  bottom: -50px;
   width: 100%;
+  background:pink;
+  padding:10px;
 `;
 
 export function Footer() {
   return (
     <FooterDiv>
-      Made with ♥ by&nbsp;<a href="https://github.com/sadanandpai/">Sadanand Akshay Pai</a>
+      Made by<span><i><b>Takshima Goyal</b></i></span>&nbsp;<span><i><b>Vibhuti Gupta</b></i></span>&nbsp;and &nbsp;<span><i><b>Yashvi Saxena</b></i></span>&nbsp;
     </FooterDiv>
   );
 }
